38f09f22d03be03999e7a9963187a6743e4d969a
Erik Bruinsma <erikbruinsma@mbp-van-erik.home>
2020-04-11 18:34:55 +0200

    laravel-varnish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel <?PHP echo time(); ?>
                 </div>
 
                 <div class="links">
