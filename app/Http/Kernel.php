496c70143efcecd66351873b91090f7c685ca997
Erik Bruinsma <erikbruinsma@mbp-van-erik.home>
2020-04-13 17:24:27 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Spatie\Varnish\Middleware\CacheWithVarnish::class,
     ];
 
     /**
