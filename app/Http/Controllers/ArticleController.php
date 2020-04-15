272d147f0452527682f44ae61ac343fc9dc6a9dd
erikbruinsma <bruinsmareclame@me.com>
2020-04-15 09:41:20 +0200

    Update ArticleController.php

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+  public function toFacebookPoster($article)
+  {
+      return with(new FacebookPosterPost($article->title))
+          ->withLink($article->getLink());
+  }
+  
   public function index()
   {
       toFacebookPoster(['title' => 'test titel', 'url' => 'https://www.google.nl', 'link' => 'https://www.google.nl']);
@@ -14,10 +20,6 @@ class ArticleController extends Controller
 
   }
 
-  public function toFacebookPoster($article)
-  {
-      return with(new FacebookPosterPost($article->title))
-          ->withLink($article->getLink());
-  }
+
 
 }
