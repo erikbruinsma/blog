be2d8c9acd82edbb115ec03a1f9f44838f60d8b2
erikbruinsma <bruinsmareclame@me.com>
2020-04-15 09:12:22 +0200

    small fix

--- database/migrations/tenant/2014_10_12_000000_create_users_table.php
+++ database/migrations/tenant/2014_10_12_000000_create_users_table.php
@@ -11,18 +11,20 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+     public function up()
+     {
+     Schema::create('users', function (Blueprint $table) {
+         $table->increments('id');
+         $table->string('name');
+         $table->string('email')->unique()->nullable();
+         $table->string('provider');
+         $table->string('provider_id');
+         $table->timestamp('email_verified_at')->nullable();
+         $table->string('password')->nullable();
+         $table->rememberToken()->nullable();
+         $table->timestamps();
+     });
+     }
 
     /**
      * Reverse the migrations.
