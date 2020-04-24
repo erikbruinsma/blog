f6b61f875373f3076901539ccf73c96652fe4e6f
erikbruinsma <bruinsmareclame@me.com>
2020-04-24 20:50:35 +0200

    Update create.blade.php

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,10 +1,7 @@
-{!! Form::open(['action' => 'Controller@method']) !!}
-
-@method('PUT')
-
-{!! Form::token() !!}
-{!! Form::text('title') !!}
-{!! Form::text('content') !!}
-{!! Form::text('published_at') !!}
-
-{!! Form::close() !!}
+<form method="post" action="{{ route('post.store') }}" />
+ @csrf
+<input type="text" name="title" value="{!! Input::old('title', '') !!}" />
+<textarea name="content" rows="3">{!! Input::old('content', '') !!}</textarea>
+<input type="text" name="published_at" value="{!! Input::old('published_at', '') !!}" />
+<input type="submit" value="opslaan" />
+</form>
