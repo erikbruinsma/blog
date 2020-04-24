67ed5796b28b6b669f4f794b614d33c6f5e05678
erikbruinsma <bruinsmareclame@me.com>
2020-04-24 22:53:11 +0200

    Update create.blade.php

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -8,10 +8,12 @@
       </div><br />
     @endif
 
+
+
     <form method="post" action="{{ route('article.store') }}" />
  @csrf
-<input type="text" name="title" value="{!! Request::old('title', '') !!}" />
-<textarea name="content" rows="3">{!! Request::old('content', '') !!}</textarea>
-<input type="text" name="published_at" value="{!! Request::old('published_at', '') !!}" />
+<input type="text" name="title" value="{!! Request::old('title', '') !!}" /><br />
+<textarea name="content" rows="3">{!! Request::old('content', '') !!}</textarea><br />
+<input type="text" name="published_at" value="{!! Request::old('published_at', '') !!}" /><br />
 <input type="submit" value="opslaan" />
 </form>
