de4630e78c7eaa1652e8c3d75943cc28ccfc1816
erikbruinsma <bruinsmareclame@me.com>
2020-04-24 20:54:56 +0200

    fix

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
 
 class ArticleController extends Controller
 {
--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,7 +1,7 @@
 <form method="post" action="{{ route('article.store') }}" />
  @csrf
-<input type="text" name="title" value="{!! Input::old('title', '') !!}" />
-<textarea name="content" rows="3">{!! Input::old('content', '') !!}</textarea>
-<input type="text" name="published_at" value="{!! Input::old('published_at', '') !!}" />
+<input type="text" name="title" value="{!! Request::old('title', '') !!}" />
+<textarea name="content" rows="3">{!! Request::old('content', '') !!}</textarea>
+<input type="text" name="published_at" value="{!! Request::old('published_at', '') !!}" />
 <input type="submit" value="opslaan" />
 </form>
