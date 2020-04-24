77858723f8e776f221d253d0641ca49cd511b46f
erikbruinsma <bruinsmareclame@me.com>
2020-04-24 22:20:29 +0200

    Update index.blade.php

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,6 +1,6 @@
 Alle artikelen ({!! $articles->count() !!}):
 
 
-foreach ($articles as $article)
+@foreach ($articles as $article)
 {!! $article->title !!} {!! $article->title_slug !!} {!! $article->content !!} {!! $article->created_at !!} <br />
 @endforeach
